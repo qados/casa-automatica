27079df4f75c903c4b16976961c8169461f1d404
qados <87865192+qados@users.noreply.github.com>
2026-05-03 13:49:08 -0300

    Sensor de chuva adicionado

--- sketch_apr13a.ino
+++ sketch_apr13a.ino
@@ -2,6 +2,7 @@
 #include <LiquidCrystal_I2C.h>
 #include <Keypad.h>
 
+// definições para o keypad e o servo motor
 LiquidCrystal_I2C lcd(0x26, 16, 2);
 Servo meuServo;
 
@@ -21,8 +22,14 @@ byte colPins[numCols] = {8, 7, 6, 5};
 Keypad myKeypad = Keypad(makeKeymap(keymap), rowPins, colPins,
                          numRows, numCols);
 
+// definições para o sensor de chuva
+int pino_a = A5;
+int pino_saida = 13;
+int limite = 200;
+
 void setup()
 {
+  // setup do lcd e servo
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
@@ -36,11 +43,19 @@ void setup()
   lcd.print("1: Abrir");
   lcd.setCursor(0, 1);
   lcd.print("2: Fechar");
+
+  // setup do sensor de chuva
+  pinMode(pino_saida, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
   char tecla = myKeypad.getKey();
+  int val_a = analogRead(pino_a);
+
+  Serial.print("Valor analogico: ");
+  Serial.println(val_a);
   
   if (tecla != NO_KEY) {
     lcd.clear();
@@ -56,5 +71,13 @@ void loop()
       lcd.setCursor(0, 1);
       lcd.print("Fechando...");
     }
+  }
+
+  if (val_a < limite) {
+    digitalWrite(pino_saida, HIGH);
+    Serial.println("Chuva suficiente - LIGADO");
+  } else {
+    digitalWrite(pino_saida, LOW);
+    Serial.println("Sem chuva suficiente");
   }
 }
